2c685d8189150601f3e5dc6fabedfab3c4d87b3b
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-05-26 20:00:34 +0200

    derive sk from seed cmd

--- src/cli.rs
+++ src/cli.rs
@@ -5,6 +5,8 @@ use crate::traits::KesSk;
 
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
 
 type GenericError = Box<dyn Error + Send + Sync + 'static>;
 type CLIResult<T> = Result<T, GenericError>;
@@ -17,12 +19,16 @@ pub enum Cmd {
 
     /// Generates 612 bytes signing key of Sum6Kes
     GenerateSk,
+
+    /// Derives 612 bytes signing key of Sum6Kes from 32 bytes seed
+    DeriveSk,
 }
 
 /// Config captured that determines what is invoked in CLI
 #[derive(Debug)]
 pub struct Config {
     cmd: Cmd,
+    file: Option<String>,
 }
 
 /// Determines and invokes Sum6Kes functions based on the parsed config
@@ -33,7 +39,6 @@ pub fn run(config: Config) -> CLIResult<()> {
             getrandom::fill(&mut seed_bytes)?;
             print!("{}", hex::encode(seed_bytes));
         }
-
         Cmd::GenerateSk => {
             let mut key_bytes = [0u8; Sum6Kes::SIZE + 4];
             let mut seed_bytes = [0u8; 32];
@@ -42,6 +47,35 @@ pub fn run(config: Config) -> CLIResult<()> {
             let mut sk_bytes = [0u8; Sum6Kes::SIZE + 4];
             sk_bytes.copy_from_slice(sk.as_bytes());
             print!("{}", hex::encode(sk_bytes));
+        }
+        Cmd::DeriveSk => {
+            match config.file {
+                None => {
+                    eprintln!("No stdin or file was provided to read a secret seed");
+                }
+                Some(seed_source) => match open_any(&seed_source) {
+                    Err(err) => {
+                        eprintln!("Failed to open {}: {}", seed_source, err);
+                    }
+                    Ok(seed_handle) => {
+                        let mut buffer = [0; 64];
+                        let mut handle = seed_handle.take(64);
+                        handle.read_exact(&mut buffer)?;
+                        match hex::decode(buffer) {
+                            Ok(bs) => {
+                                let mut seed_bytes = [0u8; 32];
+                                seed_bytes.copy_from_slice(&bs);
+                                let mut key_bytes = [0u8; Sum6Kes::SIZE + 4];
+                                let (sk, _pk) = Sum6Kes::keygen(&mut key_bytes, &mut seed_bytes);
+                                print!("{}", hex::encode(sk.as_bytes()));
+                            }
+                            Err(err) => {
+                                eprintln!("Decode error of the secret seed: {}", err);
+                            }
+                        }
+                    }
+                },
+            };
         }
     }
     Ok(())
@@ -57,27 +91,59 @@ pub fn get_args() -> CLIResult<Config> {
             Arg::with_name("generate_seed")
                 .short("s")
                 .long("generate_seed")
-                .help("Generate a secret key")
+                .help("Generate a 32-byte secret key")
                 .takes_value(false),
         )
         .arg(
             Arg::with_name("generate_sk")
                 .short("k")
                 .long("generate_sk")
-                .help("Generate a signing key")
+                .help("Generate a 612-byte signing key")
                 .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("derive_sk")
+                .short("d")
+                .long("derive_sk")
+                .help("Derive a 612-byte signing key from a 32-byte secret seed (stdin/file)")
+                .conflicts_with("generate_seed")
+                .conflicts_with("generate_sk")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("file")
+                .value_name("FILE")
+                .help("Input file")
+                .multiple(false)
+                .default_value("-"),
         )
         .get_matches();
 
     Ok(if matches.is_present("generate_seed") {
         Config {
             cmd: Cmd::GenerateSeed,
+            file: None,
         }
     } else if matches.is_present("generate_sk") {
         Config {
             cmd: Cmd::GenerateSk,
+            file: None,
+        }
+    } else if matches.is_present("derive_sk") {
+        Config {
+            cmd: Cmd::DeriveSk,
+            file: matches
+                .values_of_lossy("file")
+                .map(|mut vec| vec.pop().unwrap()),
         }
     } else {
         panic!("wrong cmd")
     })
+}
+
+fn open_any(filename: &str) -> CLIResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
