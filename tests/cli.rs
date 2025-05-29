77037baed0365a5ef31866d59c1bc8aff2876926
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-05-29 12:06:10 +0200

    generate sk output length test

--- tests/cli.rs
+++ tests/cli.rs
@@ -22,12 +22,19 @@ fn correct_output_version_arg() {
         .stdout(predicate::str::contains(ver));
 }
 
-#[test]
-fn correct_length_hex_output_generate_arg() {
+fn hex_length_check(option: &str, len: i32) {
     let mut cmd = Command::cargo_bin(PRG).unwrap();
-    let is_32_byte_hex = predicate::str::is_match("^[0-9a-f]{64}$").unwrap();
-    cmd.arg("--generate_seed")
-        .assert()
-        .success()
-        .stdout(is_32_byte_hex);
+    let regex = format!("^[0-9a-f]{{{}}}$", 2 * len);
+    let is_len_byte_hex = predicate::str::is_match(regex).unwrap();
+    cmd.arg(option).assert().success().stdout(is_len_byte_hex);
+}
+
+#[test]
+fn correct_length_output_generate_seed() {
+    hex_length_check("--generate_seed", 32)
+}
+
+#[test]
+fn correct_length_output_generate_signing_key() {
+    hex_length_check("--generate_sk", 612)
 }
