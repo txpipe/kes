5887504a90113dbf5e76b28b094b0eb5e7df5043
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-05-29 12:56:51 +0200

    adding check that deriving sk is deterministic

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,8 @@
 use assert_cmd::Command;
+use getrandom::fill;
 use predicates::prelude::*;
+use std::io::Write;
+use tempfile::NamedTempFile;
 
 const PRG: &str = "kes-summed-ed25519";
 
@@ -37,4 +40,40 @@ fn correct_length_output_generate_seed() {
 #[test]
 fn correct_length_output_generate_signing_key() {
     hex_length_check("--generate_sk", 612)
+}
+
+#[test]
+fn deriving_sk_seed_is_deterministic() {
+    let mut random_bytes = [0u8; 32];
+    let _ = fill(&mut random_bytes[..]);
+    let mut seed = NamedTempFile::new().unwrap();
+    write!(seed, "{}", hex::encode(&random_bytes)).unwrap();
+    let seed_file_name = (*seed.path()).display().to_string();
+
+    let sk1 = Command::cargo_bin(PRG)
+        .unwrap()
+        .args(["--derive_sk", &seed_file_name])
+        .assert()
+        .get_output()
+        .stdout
+        .clone();
+    let sk2 = Command::cargo_bin(PRG)
+        .unwrap()
+        .args(["--derive_sk", &seed_file_name])
+        .assert()
+        .get_output()
+        .stdout
+        .clone();
+    let sk3 = Command::cargo_bin(PRG)
+        .unwrap()
+        .write_stdin(hex::encode(&random_bytes))
+        .arg("--derive_sk")
+        .assert()
+        .get_output()
+        .stdout
+        .clone();
+    // derivation is deterministic is file is used
+    assert_eq!(sk1, sk2);
+    // derivation is deterministic irrespective of file/stdin input
+    assert_eq!(sk1, sk3);
 }
